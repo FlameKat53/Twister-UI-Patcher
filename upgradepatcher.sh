03f21b9327d3d30b37d54bd0610281d586c61ac9
FlameKat53 <flame9804@yahoo.com>
2020-10-26 10:20:09 -0500

    Update upgradepatcher.sh

--- upgradepatcher.sh
+++ upgradepatcher.sh
@@ -2,7 +2,7 @@
 #Uninstalls and reinstalls the patcher
 bash ~/patcher/uninstall.sh
 cd ${HOME}
-wget https://raw.githubusercontent.com/FlameKat53/Twister-UI-Patcher/master/install.sh
+wget https://raw.githubusercontent.com/FlameKat53/Twister-UI-Patcher/main/install.sh
 chmod +x ./install.sh
 ./install.sh
 cd ${HOME}
