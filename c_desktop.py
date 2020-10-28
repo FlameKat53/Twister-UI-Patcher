26846d42504cdc2f2b85a41fc93f6ad83a226ab9
FlameKat53 <flame9804@yahoo.com>
2020-10-28 15:09:02 -0500

    Update c_desktop.py

--- c_desktop.py
+++ c_desktop.py
@@ -7,7 +7,7 @@ home_path = expanduser("~%s" % user)
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
 print(dir_path)
-f_content = "[Desktop Entry]\nName=Twister UI Patcher\nComment=Updates Twister UI\nExec="+dir_path+"/src/start.sh\nIcon="+dir_path+"/src/icon.png\nCategories=System;\nVersion=1.0.0\nType=Application\nTerminal=false\nStartupNotify=true"
+f_content = "[Desktop Entry]\nName=Twister UI Patcher\nComment=Updates Twister UI\nExec=xfce4-terminal -e "+dir_path+"/src/start.sh\nIcon="+dir_path+"/src/icon.png\nCategories=System;\nVersion=1.0.0\nType=Application\nTerminal=false\nStartupNotify=true"
 print(f_content)
 
 x_dir = home_path+"/.local/share/applications/patcher.desktop"
