3f8c569320a2edd4cbf69a3c53d00ddd266cd342
GlasierXplor <timothy.wong@rocketmail.com>
2020-11-01 14:14:42 +0800

    Added if statement to check if '~/.local/share/applications' exists

--- c_desktop.py
+++ c_desktop.py
@@ -10,7 +10,12 @@ print(dir_path)
 f_content = "[Desktop Entry]\nName=Twister UI Patcher\nComment=Updates Twister UI\nExec="+dir_path+"/src/start.sh\nIcon="+dir_path+"/src/icon.png\nCategories=System;\nVersion=1.0.0\nType=Application\nTerminal=false\nStartupNotify=true"
 print(f_content)
 
-x_dir = home_path+"/.local/share/applications/patcher.desktop"
+app_dir = home_path+"/.local/share/applications"
+x_dir = app_dir+"/patcher.desktop"
+
+if not os.path.isdir(app_dir):
+    os.mkdir(app_dir)
+
 print("Saved menu shortcut to %s" % x_dir)
 f2 = open(x_dir, "w+")
 f2.write(f_content)
